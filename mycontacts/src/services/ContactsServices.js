ae30c162a08ded8c1520c3801157e3b30d6fa298
mtsgreat <mtsgreat@gmail.com>
2023-05-26 11:06:48 -0300

    refactory method http client

--- mycontacts/src/services/ContactsServices.js
+++ mycontacts/src/services/ContactsServices.js
@@ -10,7 +10,7 @@ class ContactsServices {
   }
 
   async createContact(contact) {
-    return this.httpClient.post('/contacts', contact);
+    return this.httpClient.post('/contacts', { body: contact });
   }
 }
 
--- mycontacts/src/services/utils/HttpClient.js
+++ mycontacts/src/services/utils/HttpClient.js
@@ -6,40 +6,43 @@ class HttpClient {
     this.baseURL = baseURL;
   }
 
-  async get(path) {
-    await delay(500);
-
-    const response = await fetch(`${this.baseURL}${path}`);
-
-    let body = null;
-    const contentType = response.headers.get('Content-Type');
-
-    if (contentType.includes('application/json')) {
-      body = await response.json();
-    }
-
-    if (response.ok) {
-      return body;
-    }
-
-    // console.log(body);
-
-    throw new APIerror(response, body);
+  get(path, options) {
+    return this.makeRequest(path, {
+      method: 'GET',
+      headers: options?.headers,
+    });
   }
 
-  async post(path, body) {
+  post(path, options) {
+    return this.makeRequest(path, {
+      method: 'POST',
+      body: options?.body,
+      headers: options?.headers,
+    });
+  }
+
+  async makeRequest(path, options) {
     await delay(500);
 
-    // const headers = new Headers({
-    //   'Content-Type': 'application/json',
-    // });
+    const headers = new Headers();
 
+    if (options.body) {
+      headers.append('Content-Type', 'application/json');
+    }
+
+    if (options.headers) {
+    //   Object.keys(options.headers).forEach((name) => {
+    //     headers.append(name, options.headers[name]);
+    //   });
+
+      Object.entries(options.headers).forEach(([name, value]) => {
+        headers.append(name, value);
+      });
+    }
     const response = await fetch(`${this.baseURL}${path}`, {
-      method: 'POST',
-      body: JSON.stringify(body),
-      headers: {
-        'Content-Type': 'application/json',
-      },
+      method: options.method,
+      body: JSON.stringify(options.body),
+      headers,
     });
 
     let responseBody = null;
